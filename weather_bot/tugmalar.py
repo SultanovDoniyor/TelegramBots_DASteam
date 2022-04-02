55da07dc5dd23a5e82af8a40cc9e29917d6a078f
SultanovDoniyor <95609798+SultanovDoniyor@users.noreply.github.com>
2022-04-02 15:24:49 +0500

    Add files via upload

---
+++ weather_bot/tugmalar.py
@@ -0,0 +1,12 @@
+from telegram import ReplyKeyboardMarkup
+
+hududlar_button=ReplyKeyboardMarkup([
+    ["Guliston", "Farg'ona"],
+    ["Andijon", "Namangan"],
+    ["Toshkent Shaxri", "Toshkent viloyati"],
+    ["Nukus", "Urganch"],
+    ["Buxoro", "Navoiy"],
+    ["Qarshi", "Termiz"],
+    ["Samarqand", "Jizzax"],
+]
+)
