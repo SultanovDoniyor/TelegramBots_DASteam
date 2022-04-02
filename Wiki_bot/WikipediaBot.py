7e54023e4b3a5ea405641d818ea900f521a5e112
SultanovDoniyor <95609798+SultanovDoniyor@users.noreply.github.com>
2022-04-02 14:41:38 +0500

    Add files via upload

---
+++ Wiki_bot/WikipediaBot.py
@@ -0,0 +1,29 @@
+import logging
+import wikipedia
+
+from aiogram import bot, Dispatcher, executor, types, Bot
+
+API_TOKEN = 'ваш токен'
+wikipedia.set_lang('uz')
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+ab = Dispatcher(bot)
+
+@ab.message_handler(commands=['start','help'])
+async def send_welcome(message: types.Message):
+
+
+    await message.reply("Assalomu-alaykum \n" 'WikipediaBotga XUSH KELIBSIZ! ')
+
+@ab.message_handler()
+async def sendWiki(message: types.Message):
+    try:
+        respond = wikipedia.summary(message.text)
+        await message.answer(respond)
+    except:
+        await message.answer("Bu mavzuga oid ma'lumot topilmadi. Iltimos qayta uruninng!")
+
+if __name__ == '__main__':
+    executor.start_polling(ab, skip_updates=True)
